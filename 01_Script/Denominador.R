533c3170df982786ea04decd9e7ab70215b20aa9
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-02 12:06:15 -0500

    Cambios

--- 01_Script/Denominador.R
+++ 01_Script/Denominador.R
@@ -1,6 +1,10 @@
 
 ## Librerías 
 
+# Revisar las librerias.
+
+install.packages(c("dplyr", "openxlsx", "readxl"))
+
 library(dplyr)
 library(openxlsx)
 library(readxl)
@@ -64,55 +68,11 @@ menores_18_2005_2019 <- menores_18_2005_2019 %>% select(4,5,96)
 
 menores_18_2020_2030 <- menores_18_2020_2030 %>% select(4,5,96)
 
-## Exportamos
+## Exportamos los Archivos
 
+write.xlsx(menores_18_2005_2019,"/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/menores_18_2005-2009.xlsx", col_names = TRUE)
 
-write.xlsx(poblacion_menores,"/Users/df.mendivelso10/Documents/GitHub/Ninez-YA/03_Process/poblacion_total_2005-2009.xlsx", col_names = TRUE)
-
-
-##### Vamos a construir la serie del Censo 2020 en adelante.
-
-
-## Filtramos la Poblacion Total elimando las observaciones que solo tienen rural o urbano
-
-CENSO_2020_2030 <- CENSO_2020_2030  %>% 
-  filter(`ÁREA GEOGRÁFICA` == "Total")
-
-
-## Eliminamos las Variables que solo tienen información sobre la población de hombres y mujeres
-## Recordemos que este DATA Set tiene un alto nivel de desagregación, nos quedamos con el valor total.
-
-censo_filtrado_2020 <- CENSO_2020_2030  %>% 
-  select(-(Hombres_0:`Mujeres_85 y más`))
-
-## Borramos las variables total hombres, total mujeres, y total general porque solo nos interesa la población
-## de 0 a 17 años.
-
-censo_filtrado_2020 <- censo_filtrado_2020   %>% 
-  select(-(`Total Hombres`:`Total`))
-
-## Vamos a crear una Nueva Variable llamada Menores, esta variable suma la población de 0 a 17 años
-
-censo_filtrado_2020 <- censo_filtrado_2020  %>% 
-  mutate(menores = rowSums(censo_filtrado_2020[ , c(7:24)]))
-
-## Obtenemos el Denomimandor base
-
-poblacion_menores_2020 <-censo_filtrado_2020 %>% select(3,5,93)
-
-### Ahora vamos a hacer el append, primero fijamos las mismas variables
-
-censo_completo_menores <- rbind(poblacion_menores_2019,poblacion_menores_2020)
-
-## Filtramos los años que necesitamos solamente
-
-censo_completo <- censo_completo %>% filter(anno >= 2005 & anno <= 2023)
-
-## Exportamos
-
-write_xlsx(df,"/Users/df.mendivelso10/Desktop/IMAGINA/VF_YA8.8.4xlsx", col_names = TRUE)
-
-getwd()
+write.xlsx(menores_18_2020_2030,"/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/menores_18_2020-2030.xlsx", col_names = TRUE)
 
 
 # ================================================
