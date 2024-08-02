8a50e5771111e4bb86f8c6be3898fcbfa30e6a2e
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-02 12:40:45 -0500

    Commit Final
    El Denominador quedó listo y comentado.

--- 01_Script/Denominador.R
+++ 01_Script/Denominador.R
@@ -20,7 +20,7 @@ CENSO_2005_2019 <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Da
 CENSO_2020_2030 <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/CENSO_2020_2030.xlsx")
 
 # ================================================
-# Aspectos Generales
+# Aspectos Generales y Limpieza Básica
 # ================================================
 
 ## La base tiene proyecciones de poblacion por Edad y Sexo, Tomamos el Total que es la suma de Urbano y Rural
@@ -35,28 +35,26 @@ CENSO_2005_2019 <- CENSO_2005_2019  %>%
 CENSO_2020_2030 <- CENSO_2020_2030  %>% 
   filter(`ÁREA GEOGRÁFICA` == "Total")
 
+# Eliminamos las Variables que solo tienen información de hombres y mujeres, dejamos solo "Total_#" que reune la población por edad
+# de hombres y mujeres.
+
+menores_2005_2019 <- CENSO_2005_2019  %>% 
+  select(-(Hombres_0:`Mujeres_85 y más`))
+
+menores_2020_2030 <- CENSO_2020_2030  %>% 
+  select(-(Hombres_0:`Mujeres_85 y más`))
 
 # ================================================
 # Sección: Menores de 18 años
 # ================================================
 
-
-# Eliminamos las Variables que solo tienen información de hombres y mujeres, dejamos solo "Total_#" que reune la población por edad
-# de hombres y mujeres.
-
-menores_18_2005_2019 <- CENSO_2005_2019  %>% 
-  select(-(Hombres_0:`Mujeres_85 y más`))
-
-menores_18_2020_2030 <- CENSO_2020_2030  %>% 
-  select(-(Hombres_0:`Mujeres_85 y más`))
-
 # Vamos a crear una Nueva Variable llamada total_menores_18, esta variable suma la población de 0 a 17 años-
 # 7 y 24 son los números de las columnas que corresponden desde Total_0 ... Total_17. 
 
-menores_18_2005_2019 <- menores_18_2005_2019  %>% 
+menores_18_2005_2019 <- menores_2005_2019  %>% 
   mutate(total_menores_18 = rowSums(menores_18_2005_2019[ , c(7:24)]))
 
-menores_18_2020_2030 <- menores_18_2020_2030  %>% 
+menores_18_2020_2030 <- menores_2020_2030  %>% 
   mutate(total_menores_18 = rowSums(menores_18_2020_2030[ , c(7:24)]))
 
 ## Vamos a construir nuestra Base Final filtrando las variables que solo necesitamos, dada la base que tenemos:
@@ -78,4 +76,28 @@ write.xlsx(menores_18_2020_2030,"/Users/daniel/Documents/GitHub/Ninez-YA/03_Proc
 # ================================================
 # Sección: Menores de 5 Años
 # ================================================
+
+# Vamos a crear una Nueva Variable llamada total_menores_5, esta variable suma la población de 0 a 4 años-
+# 7 y 11 son los números de las columnas que corresponden desde Total_0 ... Total_4. 
+
+menores_5_2005_2019 <- menores_2005_2019  %>% 
+  mutate(total_menores_5 = rowSums(menores_2005_2019[ , c(7:11)]))
+
+menores_5_2020_2030 <- menores_2020_2030  %>% 
+  mutate(total_menores_5 = rowSums(menores_2020_2030[ , c(7:11)]))
+
+## Vamos a construir nuestra Base Final filtrando las variables que solo necesitamos, dada la base que tenemos:
+# La Variable 4 = "MPIO" el identificador del Municipio
+# La Variable 5 = "AÑO" el año
+# La Variable 96 = "total_menores_18" la variable que creamos
+
+menores_5_2005_2019 <- menores_5_2005_2019 %>% select(4,5,96)
+
+menores_5_2020_2030 <- menores_5_2020_2030 %>% select(4,5,96)
+
+## Exportamos los Archivos
+
+write.xlsx(menores_5_2005_2019,"/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/menores_18_2005-2009.xlsx", col_names = TRUE)
+
+write.xlsx(menores_5_2020_2030,"/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/menores_18_2020-2030.xlsx", col_names = TRUE)
 
