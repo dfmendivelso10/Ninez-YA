e68570cd3273f75a038c3a2232d7b94c257c972a
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-04 10:55:58 -0500

    .

---
+++ 01_Script/YA_4.2.R
@@ -0,0 +1,12 @@
+## Librerías 
+
+# Revisar las librerias.
+
+install.packages(c("dplyr", "openxlsx", "readxl"))
+
+library(dplyr)
+library(openxlsx)
+library(readxl)
+
+
+YA_4.2 <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/YA_4.1.xlsx")
