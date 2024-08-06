99ab941aa52bce04ab54204f2b6b9972bf99fa8a
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-05 19:22:13 -0500

    Correciones Finales

---
+++ 01_Script/YA_1.2.R
@@ -0,0 +1,12 @@
+
+# ================================================
+# Aspectos Generales Alcantarillado
+# ================================================
+
+# Estos datos no requieren un procesamiento especializado
+# simplemente acceda al Siguiente Enlace 
+# https://wa-reportsui.azurewebsites.net//home/report/3464ae84-4886-4d51-93dd-0b28c273a40f
+# El cual contiene un reporte del Sistema Único de Información
+# de Servicios Públicos Domiciliarios, luego organice los datos
+# en Excel con el formato codmpio anno y el nombre que quiera darle
+# al indicador, en este caso Alcantarillado
