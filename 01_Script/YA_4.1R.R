2148c67e9fcfcc4da62173bbb78ec45a8e5b9ab6
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-05 19:22:13 -0500

    Correciones Finales

--- 01_Script/YA_4.1R.R
+++ 01_Script/YA_4.1R.R
@@ -13,31 +13,31 @@ library(stringr)
 
 
 
-# Cargamos Nuestra Base YA_4.2
+# Cargamos Nuestra Base YA_4.1
 
-YA_4.2 <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/YA_4.2.xlsx")
+YA_4.1 <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/YA_4.1.xlsx")
 
 # Borramos la Variable Total General
 
-YA_4.2 <- YA_4.2[ , -22]
+YA_4.1 <- YA_4.1[ , -22]
 
 # Separamos el CODMPIO del Nombre del Municipio
 
-YA_4.2$codmpio <- str_replace(YA_4.2$codmpio, " - .*", "")
+YA_4.1$codmpio <- str_replace(YA_4.1$codmpio, " - .*", "")
 
 # Organizamos la Base de Datos, estos están en Wide, de manera que
 # los vamos a convertir a Longer.
 
-YA_4.2 <- YA_4.2 %>%
+YA_4.1 <- YA_4.1 %>%
   pivot_longer(
     cols = starts_with("20"), # Seleccionamos las columnas que empiezan con "20" (años desde 2000)
     names_to = "anno", # Nuevo nombre de columna para los nombres de las columnas originales
-    values_to = "IRA" # Nuevo nombre de columna para los valores
+    values_to = "EDA" # Nuevo nombre de columna para los valores
   )
 
 # Podemos validar el formato Long
 
-head(YA_4.2)
+head(YA_4.1)
 
 # ====================================================
 # Sección: Merge Data  
@@ -45,30 +45,35 @@ head(YA_4.2)
 
 # Verificamos la Estructura de los Datos, por ejemplo
 
-class(YA_4.2$codmpio)
-class(YA_4.2$anno)
-class(YA_4.2$IRA) # Podemos hacerlo paara cada una de las variables
+class(YA_4.1$codmpio)
+class(YA_4.1$anno)
+class(YA_4.1$IRA) # Podemos hacerlo paara cada una de las variables
 class(menores_5_años$codmpio)
 class(menores_5_años$anno)
 class(menores_5_años$total_menores_5)
 
 # Cambiamos de String a Numeric
 
-YA_4.2 <- YA_4.2 %>%
+YA_4.1 <- YA_4.1 %>%
   mutate(codmpio = as.numeric(codmpio))
 
-YA_4.2 <- YA_4.2 %>%
+YA_4.1 <- YA_4.1 %>%
   mutate(anno = as.numeric(anno))
 
 # Realizamos el Inner Join
 
-YA_4.2_VF <- inner_join(menores_5_años, YA_4.2, by = c("codmpio","anno"))
+YA_4.1_VF <- inner_join(menores_5_años, YA_4.1, by = c("codmpio","anno"))
 
 # Creamos la Tasa de mortalidad por Infección Respiratoria Aguda (IRA) en menores de 5 años
 
-YA_4.2_VF <- YA_4.2_VF %>% 
-  mutate(tasa_IRA = (IRA / total_menores_5) * 100000)
-  
-  
-# Exportamos la Versión Final
+YA_4.1_VF <- YA_4.1_VF %>% 
+  mutate(tasa_EDA = (EDA / total_menores_5) * 100000)
+
+
+# Exportamos la Versión Final de Nuestro Indicador
+
+write.xlsx(YA_4.1_VF, "/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/VF_YA_4.1.xlsx", col_names = TRUE)
+
+
+
 
