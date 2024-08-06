0e22fb629bacd27248052c37d2de13f947681df4
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-05 23:42:16 -0500

    ....

--- 01_Script/YA_2.R
+++ 01_Script/YA_2.R
@@ -23,5 +23,20 @@ educ_inicial <-  read.csv("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/E
 # Limpiamos la Base de Datos
 
 educ_inicial <- educ_inicial %>%
-  mutate(FECHA = dmy(Fecha),   # Convertir la fecha de texto a formato Date
-         Año = year(Fecha))     # Extraer el año
+  mutate(FECHA = dmy(FECHA),   # Convertir la fecha de texto a formato Date
+         Año = year(FECHA))     # Extraemos el año y ya no tenemos día ni mes
+
+# Eliminamos Variables que no Necesitamos
+
+educ_inicial <- educ_inicial %>%
+  select(-1:-5,-7) # Acá Eliminamos las Primeras 5 columnas y la columna número 7
+
+# Renombramos algunas variables
+
+educ_inicial  <- educ_inicial  %>%
+  rename(anno = Año)
+
+educ_inicial  <- educ_inicial  %>%
+  rename(codmpio = COD_DANE_MUNICIPIO)
+
+
