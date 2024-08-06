14d1213441f357b6139ce9b2400d1ed1a28dcbd3
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-06 16:06:55 -0500

    Correcciones hasta el YA_1.4

---
+++ 01_Script/YA_1.4.R
@@ -0,0 +1,82 @@
+
+
+# ================================================
+# YA 1.4 Proporción de nacidos vivos con 4 o más controles prenatales
+# ================================================
+# Librerías y Paquetes
+
+install.packages(c("dplyr", "openxlsx", "readxl", "tidyr", "stringr"))
+
+
+library(tidyr)
+library(dplyr)
+library(openxlsx)
+library(readxl)
+library(stringr)
+
+
+
+# Cargamos Nuestra Base YA_1.4
+
+YA_1.4 <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/YA_1.4.xlsx")
+
+# Borramos la Variable Total General
+
+YA_1.4 <- YA_1.4[ , -21]
+
+# Separamos el CODMPIO del Nombre del Municipio
+
+YA_1.4$codmpio <- str_replace(YA_1.4$codmpio, " - .*", "")
+
+# Organizamos la Base de Datos, estos están en Wide, de manera que
+# los vamos a convertir a Longer.
+
+YA_1.4 <- YA_1.4 %>%
+  pivot_longer(
+    cols = starts_with("20"), # Seleccionamos las columnas que empiezan con "20" (años desde 2000)
+    names_to = "anno", # Nuevo nombre de columna para los nombres de las columnas originales
+    values_to = "controles_prenatales" # Nuevo nombre de columna para los valores
+  )
+
+# Podemos validar el formato Long
+
+head(YA_1.4)
+
+# ====================================================
+# Sección: Merge Data  
+# ====================================================
+
+# Verificamos la Estructura de los Datos, por ejemplo
+
+class(YA_1.4$codmpio)
+class(YA_1.4$anno)
+class(YA_1.4$controles_prenatales) # Podemos hacerlo paara cada una de las variables
+class(VF_nacidos_vivos$codmpio)
+class(VF_nacidos_vivos$anno)
+class(VF_nacidos_vivos$nacidos_vivos)
+
+# Cambiamos de String a Numeric
+
+YA_1.4 <- YA_1.4 %>%
+  mutate(codmpio = as.numeric(codmpio))
+
+YA_1.4 <- YA_1.4 %>%
+  mutate(anno = as.numeric(anno))
+
+# Realizamos el Inner Join * Cargamos el DataSet nacidos_vivos
+
+YA_1.4_VF <- inner_join(VF_nacidos_vivos, YA_1.4, by = c("codmpio","anno"))
+
+# Creamos la Tasa de mortalidad por Infección Respmortalidad_menores_5toria Aguda (mortalidad_menores_5) en menores de 5 años
+
+YA_1.4_VF <- YA_1.4_VF %>% 
+  mutate(proporcion_controles_prenatales = ( controles_prenatales / nacidos_vivos)*100) 
+
+
+# Exportamos la Versión Final de Nuestro Indicador
+
+write.xlsx(YA_1.4_VF, "/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/VF_YA_1.4.xlsx", col_names = TRUE)
+
+# Fin del Código
+
+
--- 01_Script/nacidos_vivos.R
+++ 01_Script/nacidos_vivos.R
@@ -1,7 +1,7 @@
 
 
 # ================================================
-# YA 1.3 Mortalidad en niños menores de 5 años 
+# Nacidos Vivos
 # ================================================
 # Librerías y Paquetes
 
@@ -16,67 +16,58 @@ library(stringr)
 
 
 
-# Cargamos Nuestra Base YA_1.3
+# Cargamos Nuestra Base nacidos_vivos
 
-YA_1.3 <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/YA_1.3.xlsx")
+nacidos_vivos <- read_excel("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/nacidos_vivos.xlsx")
 
 # Borramos la Variable Total General
 
-YA_1.3 <- YA_1.3[ , -21]
+nacidos_vivos <- nacidos_vivos[ , -21]
 
 # Separamos el CODMPIO del Nombre del Municipio
 
-YA_1.3$codmpio <- str_replace(YA_1.3$codmpio, " - .*", "")
+nacidos_vivos$codmpio <- str_replace(nacidos_vivos$codmpio, " - .*", "")
 
 # Organizamos la Base de Datos, estos están en Wide, de manera que
 # los vamos a convertir a Longer.
 
-YA_1.3 <- YA_1.3 %>%
+nacidos_vivos <- nacidos_vivos %>%
   pivot_longer(
     cols = starts_with("20"), # Seleccionamos las columnas que empiezan con "20" (años desde 2000)
     names_to = "anno", # Nuevo nombre de columna para los nombres de las columnas originales
-    values_to = "mortalidad_menores_5" # Nuevo nombre de columna para los valores
+    values_to = "nacidos_vivos" # Nuevo nombre de columna para los valores
   )
 
 # Podemos validar el formato Long
 
-head(YA_1.3)
+head(nacidos_vivos)
 
 # ====================================================
-# Sección: Merge Data  
+# Sección: Limpieza y Adecuación 
 # ====================================================
 
 # Verificamos la Estructura de los Datos, por ejemplo
 
-class(YA_1.3$codmpio)
-class(YA_1.3$anno)
-class(YA_1.3$mortalidad_menores_5) # Podemos hacerlo paara cada una de las variables
+class(nacidos_vivos$codmpio)
+class(nacidos_vivos$anno)
+class(nacidos_vivos$mortalidad_menores_5) # Podemos hacerlo paara cada una de las variables
 class(menores_5_años$codmpio)
 class(menores_5_años$anno)
 class(menores_5_años$total_menores_5)
 
 # Cambiamos de String a Numeric
 
-YA_1.3 <- YA_1.3 %>%
+nacidos_vivos <- nacidos_vivos %>%
   mutate(codmpio = as.numeric(codmpio))
 
-YA_1.3 <- YA_1.3 %>%
+nacidos_vivos <- nacidos_vivos %>%
   mutate(anno = as.numeric(anno))
-
-# Realizamos el Inner Join * Cargamos el DataSet menores_5_años
-
-YA_1.3_VF <- inner_join(menores_5_años, YA_1.3, by = c("codmpio","anno"))
-
-# Creamos la Tasa de mortalidad por cualquier condición
-
-YA_1.3_VF <- YA_1.3_VF %>% 
-  mutate(tasa_mortalidad_menores_5 = (mortalidad_menores_5 / total_menores_5) * 1000)
-
-
-# Exportamos la Versión Final de Nuestro Indicador
-
-write.xlsx(YA_1.3_VF, "/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/VF_YA_1.3.xlsx", col_names = TRUE)
+         
+# Exportamos la Versión Final de Nuestro Indicador de Nacidos Vivos
+         
+write.xlsx(nacidos_vivos, "/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/VF_nacidos_vivos.xlsx", col_names = TRUE)
 
 # Fin del Código
-
-
+         
+         
+         
