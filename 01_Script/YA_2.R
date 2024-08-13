cb88b5cdb429f2b3274fc40fcc369898b97ced9c
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-13 10:51:34 -0500

    Update YA_2.R

--- 01_Script/YA_2.R
+++ 01_Script/YA_2.R
@@ -43,28 +43,6 @@ educ_inicial  <- educ_inicial  %>%
 # Creación de Sub - Data Sets
 # ================================================
 
-# Primero renombramos las variables
 
-educ_inicial <- educ_inicial %>% rename(educ_inicial_icbf = INDICADOR.1..EDUCACIÓN.INICIAL.ICBF..Incluye.Mujeres.Gestantes.)
-
-educ_inicial <- educ_inicial %>% rename(educ_inicial_marco_integral = INDICADOR.2..NIÑOS.Y.NIÑAS.EN.PREESCOLAR.CON.EDUCACIÓN.INICIAL)
-
-educ_inicial <- educ_inicial %>% rename(porcentaje_marco_integral = INDICADOR.4..CONCURRENCIA.DE.ATENCIONES)
-
-# Definimos los Datasets
-
-educ_inicial_ICBF <- subset(educ_inicial, selec = c(codmpio, anno, educ_inicial_icbf))
-
-educ_inicial_marco_integral <- subset(educ_inicial, selec = c(codmpio, anno, educ_inicial_marco_integral))
-
-porcentaje_marco_integral <- subset(educ_inicial, selec = c(codmpio, anno, porcentaje_marco_integral))
-
-# Exportamos los Datasets
-
-write.xlsx(educ_inicial_ICBF, file = "~/GitHub/Ninez-YA/03_Process/educ_inicial_ICBF.xlsx", col_names = TRUE)
-
-write.xlsx(educ_inicial_marco_integral, file = "~/GitHub/Ninez-YA/03_Process/educ_inicial_marco_integral.xlsx", col_names = TRUE)
-
-write.xlsx(porcentaje_marco_integral, file = "~/GitHub/Ninez-YA/03_Process/porcentaje_marco_integral.xlsx", col_names = TRUE)
 
 
