7c54721906d5aa62920506b253224026343131a9
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-08-19 11:53:06 -0500

    Update YA_3.R

--- 01_Script/YA_3.R
+++ 01_Script/YA_3.R
@@ -26,9 +26,9 @@ educ_inicial <-  read.csv("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/p
 educ_inicial <- educ_inicial %>%
   select(-3:-8) # Acá Eliminamos las Primeras 6 columnas y la columna número 8
 
-# Renombrammos algunas variables
+# Renombramos algunas variables
 
-educ_inicial <- educ_inicial %>% rename(anno = AÑO)
+educ_inicial <- educ_inicial %>% rename(anno = AÑO) 
 
 educ_inicial <- educ_inicial %>% rename(codmpio = CÓDIGO_MUNICIPIO)
 
@@ -61,24 +61,32 @@ educ_inicial <- educ_inicial %>% rename(desercion_media= DESERCIÓN_MEDIA)
 # Creación de Sub - Data Sets
 # ================================================
 
-# YA 2.1 Cobertura Neta Transición
+# YA 3.1 Cobertura Neta 
 
 YA_3.4_VF <- subset(educ_inicial, select = c("anno", "codmpio","neta_primaria"))
 
-YA_3.4_VF <- subset(educ_inicial, select = c("anno", "codmpio","neta_media"))
+YA_3.5_VF <- subset(educ_inicial, select = c("anno", "codmpio","neta_media"))
+
+# YA 3.6-7 Reprobación Neta 
+
+YA_3.6_VF <- subset(educ_inicial, select = c("anno", "codmpio","reprobacion_primaria"))
+
+YA_3.7_VF <- subset(educ_inicial, select = c("anno", "codmpio","reprobacion_media"))
+
+# YA 3.8-9 Repitencia Neta 
+
+YA_3.8_VF <- subset(educ_inicial, select = c("anno", "codmpio","repitencia_primaria"))
+
+YA_3.9_VF <- subset(educ_inicial, select = c("anno", "codmpio","repitencia_media"))
+
+# YA 3.10-11 Deserción Neta 
+
+YA_3.10_VF <- subset(educ_inicial, select = c("anno", "codmpio","desercion_primaria"))
+
+YA_3.11_VF <- subset(educ_inicial, select = c("anno", "codmpio","desercion_media"))
 
 
 
-
-
-
-
-
-YA_2.2_VF  <- subset(educ_inicial, select = c("anno", "codmpio","reprobacion_transicion"))
-
-YA_2.3_VF  <- subset(educ_inicial, select = c("anno", "codmpio","repitencia_transicion"))
-
-YA_2.4_VF <- subset(educ_inicial, select = c("anno", "codmpio","desercion_transicion"))
 
 # ================================================
 # Exportamos Data Sets
