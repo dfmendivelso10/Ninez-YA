9686a2fa650d95c1834e6a17cfb2e48e5b949ea6
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-09-16 07:41:24 -0500

    ...

---
+++ 01_Script/YA_3_Saber_11.R
@@ -0,0 +1,15 @@
+# ================================================
+# YA 3 Saber 11
+# ================================================
+# Librerías y Paquetes
+
+install.packages(c("dplyr", "openxlsx", "readxl", "tidyr", "stringr"))
+
+
+library(tidyr)
+library(dplyr)
+library(openxlsx)
+library(readxl)
+library(stringr)
+
+
