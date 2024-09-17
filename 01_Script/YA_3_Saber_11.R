82652180f123ff836dfaac33a83a2d1e1b185e94
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-09-16 23:10:10 -0500

    Update YA_3_Saber_11.R

--- 01_Script/YA_3_Saber_11.R
+++ 01_Script/YA_3_Saber_11.R
@@ -29,47 +29,36 @@ X2020 <- read_delim("Downloads/Raw_DATA/2020.txt", delim = "/", escape_double = 
 
 X2021 <- read_delim("Downloads/Raw_DATA/2021.txt", delim = "/", escape_double = FALSE, trim_ws = TRUE)
 
+## Convertimos los Datos a data.table * Es más eficiente con el uso de la memoria
+
+X2016 <- as.data.table(X2016)
+X2017 <- as.data.table(X2017)
+X2018 <- as.data.table(X2018)
+X2019 <- as.data.table(X2019)
+X2020 <- as.data.table(X2020)
+X2021 <- as.data.table(X2021)
+
+# Usar rbindlist para concatenar todas las bases de datos
+saber_11 <- rbindlist(list(X2016, X2017, X2018, X2019, X2020, X2021), fill = TRUE)
 
 
 # Limpiamos las Bases  de Datos
+saber_11 <- saber_11[ , -c(2:3, 5:13, 15:27)]
 
-X2016 <- X2016[ , -c(2:3,5:13,15:27)]
-X2017 <- X2017[ , -c(2:3,5:13,15:27)]
-X2018 <- X2018[ , -c(2:3,5:13,15:27)]
-X2019 <- X2019[ , -c(2:3,5:13,15:27)]
-X2020 <- X2020[ , -c(2:3,5:13,15:27)]
-X2021 <- X2021[ , -c(2:3,5:13,15:27)]
 
 # Rename de Variables
 
-X2016 <- rename(X2016, codmpio = codigodane_municipio, anno = periodo)
-X2017 <- rename(X2017, codmpio = codigodane_municipio, anno = periodo)
-X2018 <- rename(X2018, codmpio = codigodane_municipio, anno = periodo)
-X2019 <- rename(X2019, codmpio = codigodane_municipio, anno = periodo)
-X2020 <- rename(X2020, codmpio = codigodane_municipio, anno = periodo)
-X2021 <- rename(X2021, codmpio = codigodane_municipio, anno = periodo)
+saber_11 <- rename(saber_11, codmpio = codigodane_municipio, anno = periodo)
 
-# Colapsa la Memoria, vamos a manejarlo como data.table
+# Conservar solo los primeros 4 dígitos de la variable año * Esto es una pequeña correción
 
-install.packages("data.table")
-library(data.table)
+saber_11$anno <- substr(saber_11$anno, 1, 4)
 
 
-# Merge de Bases
 
-
-# Definir las columnas clave para hacer el merge
-columnas_clave <- c("anno", "codmpio", "nombre_prueba")
-
-# Usar Reduce para combinar las bases de datos de X2016 a X2021 usando las columnas clave
-saber_11 <- Reduce(function(x, y) merge(x, y, by = columnas_clave, all = TRUE), list(X2016, X2017, X2018, X2019, X2020, X2021))
-
-
-# Calcular el promedio ponderado por codigodane_municipio y nombre_prueba
-
-promedios_por_municipio <- X2016 %>%
+promedios_por_municipio <- saber_11 %>%
   filter(!is.na(promedio_prueba)) %>%  # Filtrar datos con promedio_prueba no NA
-  group_by(codigodane_municipio, nombre_prueba) %>%
+  group_by(anno, codmpio, nombre_prueba) %>%
   summarize(
     promedio_ponderado = sum(promedio_prueba * n_evaluados, na.rm = TRUE) / sum(n_evaluados, na.rm = TRUE)
   ) %>%
