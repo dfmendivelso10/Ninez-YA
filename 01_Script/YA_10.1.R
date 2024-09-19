b24c01d7c75c828f59adff73cb3f7c8084cfc750
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-09-18 23:40:03 -0500

    .....

--- 01_Script/YA_10.1.R
+++ 01_Script/YA_10.1.R
@@ -17,41 +17,52 @@ library(data.table)
 library(readr)
 
 
-# Función ajustada para manejar diferentes nombres de la columna
-read_and_prepare <- function(file, sheet, year) {
-  data <- read_excel(file, sheet = sheet, col_names = TRUE) %>%
-    select(-starts_with("...")) %>%  # Eliminar columnas sin nombre
-    
-    # Manejar diferencias en el nombre de la columna `Periodo del Indicador`
-    mutate(`Periodo del Indicador` = if ("Periodo del Indicador" %in% colnames(.)) {
-      as.character(`Periodo del Indicador`)
-    } else if ("Periodo del indicador" %in% colnames(.)) {
-      as.character(`Periodo del indicador`)
-    } else {
-      NA  # Si no existe, crear la columna con NA
-    },
-    ano = year)  # Agregar la columna del año
-  
-  return(data)
-}
 
 # Cargamos los datos ajustados con la hoja "Ind. Patología"
-procu_2015 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2015.xlsx", "Ind. Patología", 2015)
-procu_2016 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2016.xlsx", "Ind. Patología", 2016)
-procu_2017 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2017.xlsx", "Ind. Patología", 2017)
-procu_2018 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2018.xlsx", "Ind. Patología", 2018)
-procu_2019 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2019.xlsx", "Ind. Patología", 2019)
-procu_2020 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2020.xlsx", "Ind. Patología", 2020)
-procu_2021 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2021.xlsx", "Ind. Patología", 2021)
-procu_2022 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2022.xlsx", "Ind. Patología", 2022)
-procu_2023 <- read_and_prepare("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2023.xlsx", "Ind. Patología", 2023)
+
+procu_2015 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2015.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2016 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2016.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2017 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2017.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2018 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2018.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2019 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2019.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2020 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2020.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2021 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2021.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2022 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2022.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
+
+procu_2023 <- read_excel("Documents/GitHub/Ninez-YA/02_RAW-Data/Procuraduria/Procuraduría_2023.xlsx", 
+                         sheet = "Ind. Patología", na = "N/A")
 
 # ====================================================
 # Sección: Merge Data  
 # ====================================================
 
-# Usamos rbindlist para combinar los dataframes y permitir columnas faltantes
-procuraduria <- rbindlist(list(procu_2015, procu_2016, procu_2017, procu_2018, procu_2019, procu_2020, procu_2021, procu_2022, procu_2023), fill = TRUE)
+
+# Crear una lista con todos los data frames
+
+procu_list <- list(procu_2015, procu_2016, procu_2017, procu_2018, procu_2019, procu_2020, procu_2021, procu_2022, procu_2023)
+
+# Encontrar las columnas comunes
+
+columnas_comunes <- Reduce(intersect, lapply(procu_list, names))
+
+# Aplicar rbindlist directamente sobre la lista de data frames filtrados por columnas comunes
+
+procuraduria <- rbindlist(lapply(procu_list, function(x) x[, columnas_comunes, with = FALSE]))
 
 # Limpiamos Memoria
 rm(procu_2015, procu_2016, procu_2017, procu_2018, procu_2019, procu_2020, procu_2021, procu_2022, procu_2023)
@@ -66,9 +77,10 @@ procuraduria <- procuraduria[, c(2, 4, 6, 7, 8, 9, 10)]
 
 procuraduria   <- rename(procuraduria , codmpio = `Código Municipio`, anno = `Periodo del Indicador`)
 
+procuraduria$anno <- str_sub(procuraduria$anno, 1, 4)
 
 # ====================================================
-# Sección: Filtrar 
+# Sección: Filtrar Indicador
 # ====================================================
 
 
@@ -77,8 +89,6 @@ homicidios <- procuraduria %>%
          `Rangos de edad o edades simples` == "(01 a 05)")
 
 
-
-homicidios$`Rangos de edad o edades simples`
 homicidios <- homicidios[, c(1,3,5,6,7)]
 
 
