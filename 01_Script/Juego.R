ec0157aaf00170dd0ac9867b1f3b24e19c9e610f
Daniel Mendivelso <69377751+dfmendivelso10@users.noreply.github.com>
2024-10-04 15:44:32 -0500

    ...

--- 01_Script/Juego.R
+++ 01_Script/Juego.R
@@ -17,15 +17,15 @@ library(tidyr)
 
 
 
-main_2023 <- read_dta("Downloads/Variables diseno muestral/muestral_2023.DTA")
+main_2023 <- read_dta("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/ECV/muestral_2023.dta")
 
-atencion_2023 <- read_dta("D:/enfluj.dta")
+integral_2023 <- read_dta("/Users/daniel/Documents/GitHub/Ninez-YA/02_RAW-Data/ECV/integral_2023.dta")
 
 
 
 # En este paso vamos a delimitar la Base de Datos, además vamos a eliminar los duplicados, en este caso los duplicados  y hacer el join en una sola línea
 
-ECV_2023 <- atencion_2023 %>%
+ecv_2023 <- integral_2023 %>%
   select(1:6) %>%  # Selecciona las primeras 6 columnas
   left_join(                                         
     main_2023 %>% distinct(DIRECTORIO, .keep_all = TRUE),  # Elimina duplicados en `main_2023`
@@ -36,7 +36,8 @@ ECV_2023 <- atencion_2023 %>%
 
 
 # Calcular el porcentaje ponderado de cada categoría de P51 para cada codmpio
-porcentaje_ponderado <- ECV_2023 %>%
+
+ecv_2023 <- ECV_2023 %>%
   group_by(codmpio, P51) %>%  # Agrupar por codmpio y categoría de P51
   summarise(frecuencia_ponderada = sum(FEX_C.x, na.rm = TRUE)) %>%  # Calcular la suma ponderada
   mutate(porcentaje = frecuencia_ponderada / sum(frecuencia_ponderada) * 100) %>%  # Calcular el porcentaje
--- 01_Script/Juego_2019-2022.R
+++ 01_Script/Juego_2019-2022.R
@@ -64,34 +64,55 @@ ecv_2020 <- integral_2020 %>%
     by = "DIRECTORIO"  # Realiza la unión usando `DIRECTORIO`
   )
 
+## No hice un loop porque el nombre de las variables en algunos casos cambia o también el orden de la variable
+# en el dataset
+
 ecv_2021 <- integral_2021 %>%
+  select(1:6) %>%  # Selecciona las primeras 6 columnas de `integral_2021`
+  left_join(
+    main_2021 %>% 
+      select(directorio, p1_departamento),  # Selecciona `directorio` y `p1_departamento` de `main_2021`
+    by = "directorio"  # Realiza la unión usando `directorio`
+  ) %>%
+  rename(DIRECTORIO = directorio, P1_DEPARTAMENTO = p1_departamento, P51 = p51, FEX_C = fex_c)  # Renombra las columnas después de la unión
+
+
+ecv_2022 <- integral_2022 %>%
   select(1:6) %>%  # Selecciona las primeras 5 columnas de `integral_2019`
   left_join(
-    main_2021 %>% select(DIRECTORIO, P1_DEPARTAMENTO),  # Selecciona `DIRECTORIO` y `P1_DEPARTAMENTO` de `main_2019`
+    main_2022 %>% select(DIRECTORIO, P1_DEPARTAMENTO),  # Selecciona `DIRECTORIO` y `P1_DEPARTAMENTO` de `main_2019`
     by = "DIRECTORIO"  # Realiza la unión usando `DIRECTORIO`
   )
 
 
-
-
-
-
-
+#===============================================================================
 # Calcular el porcentaje ponderado de cada categoría de P51 para cada codmpio
+#===============================================================================
 
 ecv_2019 <- ecv_2019 %>%
   group_by(P1_DEPARTAMENTO, P51) %>%  # Agrupar por coddepto y categoría de P51
   summarise(frecuencia_ponderada = sum(FEX_C, na.rm = TRUE)) %>%  # Calcular la suma ponderada
   mutate(porcentaje = frecuencia_ponderada / sum(frecuencia_ponderada) * 100) %>%  # Calcular el porcentaje
+  ungroup() %>% 
+  select()# Desagrupar el resultado final
+
+ecv_2020 <- ecv_2020 %>%
+  group_by(P1_DEPARTAMENTO, P51) %>%  # Agrupar por coddepto y categoría de P51
+  summarise(frecuencia_ponderada = sum(FEX_C, na.rm = TRUE)) %>%  # Calcular la suma ponderada
+  mutate(porcentaje = frecuencia_ponderada / sum(frecuencia_ponderada) * 100) %>%  # Calcular el porcentaje
   ungroup()  # Desagrupar el resultado final
 
+ecv_2021 <- ecv_2021 %>%
+  group_by(P1_DEPARTAMENTO, P51) %>%  # Agrupar por coddepto y categoría de P51
+  summarise(frecuencia_ponderada = sum(FEX_C, na.rm = TRUE)) %>%  # Calcular la suma ponderada
+  mutate(porcentaje = frecuencia_ponderada / sum(frecuencia_ponderada) * 100) %>%  # Calcular el porcentaje
+  ungroup()  # Desagrupar el resultado final
 
-
-
-
-
-
-
+ecv_2022 <- ecv_2022 %>%
+  group_by(P1_DEPARTAMENTO, P51) %>%  # Agrupar por coddepto y categoría de P51
+  summarise(frecuencia_ponderada = sum(FEX_C, na.rm = TRUE)) %>%  # Calcular la suma ponderada
+  mutate(porcentaje = frecuencia_ponderada / sum(frecuencia_ponderada) * 100) %>%  # Calcular el porcentaje
+  ungroup()  # Desagrupar el resultado final
 
 
 # Exportamos
