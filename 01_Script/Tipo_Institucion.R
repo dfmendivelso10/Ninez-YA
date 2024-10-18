138df74638bc892ae87753745db18b5651d1babf
Daniel Mendivelso <df.mendivelso10@gmail.com>
2024-10-18 17:47:48 -0500

    Comentarios

--- 01_Script/Tipo_Institucion.R
+++ 01_Script/Tipo_Institucion.R
@@ -16,38 +16,39 @@ library(lubridate) # Nos permite manejar fechas.
 
 # Cargamos Nuestra Base 
 
-tipo_institutcion <-  read.csv("~/Downloads/MEN_ESTABLECIMIENTOS_EDUCATIVOS_PREESCOLAR_B_SICA_Y_MEDIA_20241018.csv")
+tipo_institucion <-  read.csv("~/Downloads/MEN_ESTABLECIMIENTOS_EDUCATIVOS_PREESCOLAR_B_SICA_Y_MEDIA_20241018.csv")
 
 # Limpiamos la Base de Datos
 
 
 # Primero eliminamos las variables que No Necesitamos
 
-tipo_institutcion  <- tipo_institutcion  %>%
+tipo_institucion  <- tipo_institutcion  %>%
   select(1,6,11) # Acá Eliminamos las observaciones que no se necesitan
 
 # Renombrammos algunas variables
 
-tipo_institutcion <- tipo_institutcion %>% rename(anno = AÑO)
+tipo_institucion <- tipo_institucion %>% rename(anno = AÑO)
 
-tipo_institutcion <- tipo_institutcion %>% rename(codmpio = COD_DANE_MUNICIPIO)
+tipo_institucion <- tipo_institucion %>% rename(codmpio = COD_DANE_MUNICIPIO)
 
-tipo_institutcion <- tipo_institutcion %>% rename(sector = SECTOR)
+tipo_institucion <- tipo_institucion %>% rename(sector = SECTOR)
 
 
 # Calculamos la Cobertura
 
 
-resultado <- tipo_institutcion %>%
+YA_3.12 <- tipo_institutcion %>%
   group_by(anno, codmpio, sector) %>%
   summarise(conteo = n(), .groups = "drop") %>%
   group_by(anno, codmpio) %>%
-  mutate(porcentaje = (conteo / sum(conteo)) * 100)
+  mutate(porcentaje = (conteo / sum(conteo)) * 100) %>%
+  rename(tipo = porcentaje)
 
 
 # Exportamos
 
-write.xlsx(YA_2.1_VF, "/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/YA_2.1.xlsx", col_names = TRUE)
+write.xlsx(YA_3.12, "/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/YA_3.12.xlsx", col_names = TRUE)
 
 
 
