e1d6daeabe75c78e5bfe4c2305049e3ecbb40b1a
Daniel Mendivelso <df.mendivelso10@gmail.com>
2024-11-22 07:01:32 -0500

    Ajustes en el YA_1.7
    Ajustes en el YA_1.7

--- 01_Script/YA_1.7.R
+++ 01_Script/YA_1.7.R
@@ -47,32 +47,6 @@ YA_1.7 <- YA_1.7 %>%
   filter(codmpio != "Total general")
 
 
-#------------------------------------------
-# Vamos a limpiar el denominador
-#------------------------------------------
-
-
-# Separamos el CODMPIO del Nombre del Municipio
-
-nacidos$codmpio <- str_replace(nacidos$codmpio, " - .*", "")
-
-
-# Organizamos la Base de Datos, estos están en Wide, de manera que
-# los vamos a convertir a Longer.
-
-nacidos <- nacidos %>%
-  pivot_longer(
-    cols = starts_with("20"), # Seleccionamos las columnas que empiezan con "20" (años desde 2000)
-    names_to = "anno", # Nuevo nombre de columna para los nombres de las columnas originales
-    values_to = "nacidos" # Nuevo nombre de columna para los valores
-  )
-
-# Borramos Total General de Codmpio
-
-nacidos <- nacidos %>%
-  filter(codmpio != "Total general")
-
-
 # ====================================================
 # Sección: Merge Data  
 # ====================================================
@@ -81,7 +55,7 @@ nacidos <- nacidos %>%
 
 class(YA_1.7$codmpio)
 class(YA_1.7$anno)
-class(YA_1.7$mortalidad_menores_1_año) # Podemos hacerlo paara cada una de las variables
+class(YA_1.7$mortalidad_neonatal) # Podemos hacerlo paara cada una de las variables
 
 # Cambiamos de String a Numeric
 
@@ -93,21 +67,32 @@ YA_1.7 <- YA_1.7 %>%
 
 # Realizamos el Inner Join * Cargamos el DataSet nacidos_vivos
 
-YA_1.7 <- inner_join(nacidos, YA_1.7, by = c("codmpio","anno"))
+YA_1.7 <- inner_join(nacidos_vivos, YA_1.7, by = c("codmpio","anno"))
 
 # Creamos la Tasa de Mortalidad por Desnutricion Aguda en Menores
 
 # Crear una nueva variable 'tasa_mortalidad_neonatal_corregida' sin alterar las variables originales
+
+
+# Limpiar y recalcular la tasa de mortalidad neonatal
+
 YA_1.7 <- YA_1.7 %>%
   mutate(
-    # Calcular la tasa de mortalidad neonatal corregida
-    tasa_mortalidad_neonatal= ifelse(
-      nacimientos > 0 & mortalidad_neonatal <= nacimientos,  # Calcular tasa solo si nacidos > 0 y mortalidad_neonatal <= nacidos
-      (mortalidad_neonatal / nacimientos) * 1000,        # Fórmula de la tasa por cada 1000 nacidos
-      NA                                            # Asignar NA si no se cumple la condición
+    # Regla 1: Si nacimientos > 0 y mortalidad_neonatal <= nacimientos, calcular la tasa
+    tasa_mortalidad_neonatal = ifelse(
+      nacimientos > 0 & mortalidad_neonatal <= nacimientos,
+      (mortalidad_neonatal / nacimientos) * 1000,
+      NA_real_  # Si no cumple la regla, asignar NA
+    ),
+    # Regla 2: Invalidar tasas iguales a 1000 cuando mortalidad_neonatal / nacimientos = 1
+    tasa_mortalidad_neonatal_corregida = ifelse(
+      mortalidad_neonatal == nacimientos & nacimientos == 1,
+      NA_real_,  # Asignar NA para casos donde mortalidad_neonatal = nacimientos = 1
+      tasa_mortalidad_neonatal_corregida
     )
   )
 
+# Mostrar los datos procesados
 
 write.xlsx(YA_1.7, "/Users/daniel/Documents/GitHub/Ninez-YA/03_Process/YA_1.7.xlsx", col_names = TRUE)
 
